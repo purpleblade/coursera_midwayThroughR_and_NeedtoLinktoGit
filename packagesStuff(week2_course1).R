9dc020cac3729d1d1d3053b5e27ce84c56e503fc
Stevie H <sehecmanczuk@crimson.ua.edu>
2021-04-15 13:59:11 -0500

    Initial commit

---
+++ packagesStuff(week2_course1).R
@@ -0,0 +1,17 @@
+m1vBeta <- diag(m1sig*solve(t(model1x)%*%model1x))
+remove.packages("ggplot2")
+
+install.packages("ggplot2")
+help(package = "ggplot2")
+browseVignettes("ggplot2")
+
+
+install.packages("devtools")
+help(package = "devtools")
+library(devtools)
+install_github
+aes
+
+
+install.packages("KernSmooth")
+library(KernSmooth)
